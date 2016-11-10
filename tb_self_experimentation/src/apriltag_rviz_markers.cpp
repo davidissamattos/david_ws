bde3f979ac8a7de2414fb6fb868b9468cacd147c
David Issa Mattos <issamattos.david@gmail.com>
2016-11-10 09:43:42 +0100

    new launch files

--- tb_self_experimentation/src/apriltag_rviz_markers.cpp
+++ tb_self_experimentation/src/apriltag_rviz_markers.cpp
@@ -18,7 +18,7 @@ uint32_t shape = visualization_msgs::Marker::CUBE;
 void updateMarkers(const geometry_msgs::Pose2D &msg)
 {
 	tf::Quaternion qt = tf::Quaternion();
-	qt.setRPY(0,0,M_PI/2-msg.theta);
+	qt.setRPY(0,0,msg.theta);
 	visualization_msgs::Marker marker;
 	marker.header.frame_id = "/map";
 	marker.header.stamp = ros::Time::now();
@@ -36,8 +36,8 @@ void updateMarkers(const geometry_msgs::Pose2D &msg)
 	cout << marker.pose <<endl;
 
 	// Set the scale of the marker -- 1x1x1 here means 1m on a side
-	marker.scale.x = 0.2;
-	marker.scale.y = 0.1;
+	marker.scale.x = 0.1;
+	marker.scale.y = 0.2;
 	marker.scale.z = 1.0;
 
 	// Set the color -- be sure to set alpha to something non-zero!
