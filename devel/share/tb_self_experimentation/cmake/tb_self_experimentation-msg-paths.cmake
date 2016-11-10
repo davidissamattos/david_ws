5b841ea73ae716bdc90fa17cf13640bdf4c31388
David Issa Mattos <issamattos.david@gmail.com>
2016-11-10 09:15:21 +0100

    new launch files

--- devel/share/tb_self_experimentation/cmake/tb_self_experimentation-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(tb_self_experimentation_MSG_INCLUDE_DIRS "/home/davidis/david_ws/src/tb_self_experimentation/msg")
-set(tb_self_experimentation_MSG_DEPENDENCIES std_msgs;geometry_msgs;move_base_msgs)
