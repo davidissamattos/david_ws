7a9901aa21b12e4fd3310bd9399c95ffbf6f97dd
David Issa Mattos <issamattos.david@gmail.com>
2016-11-16 09:27:35 +0100

    synchronizing last changes and bug corrections

--- apriltags/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davidis/david_ws/src/apriltags
-# Build directory: /home/davidis/david_ws/src/apriltags
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- tb_self_experimentation/pynodes/metric_analysis.py
+++ tb_self_experimentation/pynodes/metric_analysis.py
@@ -39,8 +39,6 @@ class MetricAnalysis:
 		#Defining the services
 		#This service will be responsible for when we get data from the feedback.py we can calculate the value, save the data and learn
 		self.service = rospy.Service('hri_distance/analyze_feedback', analyze_feedback, self.analyzeFeedbackService)
-
-
 		#Dont let the node die
     		rospy.spin()
 	
